583bbff1755a34ba025ae5573d6156813901c454
Andrew Trac <andrewtrac87@hotmail.com>
2020-05-25 21:54:39 +0000

    New queries for total assignment duration

---
+++ 2_queries_joins/cohort_assignment_duration.sql
@@ -0,0 +1,4 @@
+SELECT SUM(assignment_submissions.duration)  as total_cohort_duration
+FROM assignment_submissions JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id  
+WHERE cohorts.name LIKE 'FEB12';
